5e3a8b806fb4f63ec968112cb842b2682e5e9426
Jake Lever <jlever@bcgsc.ca>
2018-07-13 16:30:54 -0700

    Add a few comments to render code

--- render.R
+++ render.R
@@ -1,8 +1,9 @@
 library(bookdown)
 library(tinytex)
 
+# Commands for building the thesis as PDF or Word
 bookdown::render_book("index.Rmd", "bookdown::pdf_book", clean = FALSE)
 bookdown::render_book("index.Rmd", "bookdown::word_document2")
 
+# Useful for debugging weird Latex issues (compile directly from the latex file)
 tinytex::pdflatex('_book/thesis.tex')
-#tinytex::pdflatex('thesis.tex')
